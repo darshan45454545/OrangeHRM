3d298b8904b15fc3401b22d77c7e003e0c45662e
darshan45 <darshanbhosale5555@gmail.com>
2019-03-20 11:55:02 +0530

    First git commit

---
+++ src/test/java/OrangeHRM1/OrangeHRM1/AppTest.java
@@ -0,0 +1,20 @@
+package OrangeHRM1.OrangeHRM1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
